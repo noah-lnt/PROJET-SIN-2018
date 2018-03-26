33dc06c48b77c36af24ecf30ebd4b32e6f6479bb
Noah Lanfrit <noah.lanfrit@hotmail.com>
2018-03-26 11:40:58 +0200

    Mise à jour N°2

---
+++ Divers/Antidémarrage du scooter .mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1516884508804" ID="ID_867998020" MODIFIED="1516884568975" TEXT="Antid&#xe9;marrage du scooter "/>
+</map>
---
+++ Divers/Capteur sécurité scooter.mm
@@ -0,0 +1,45 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1516883421751" ID="ID_1158456799" MODIFIED="1516964118990" TEXT="Capteur s&#xe9;curit&#xe9; scooter">
+<node CREATED="1516883431257" ID="ID_1143599405" MODIFIED="1516964040360" POSITION="right" TEXT="Acc&#xe9;l&#xe9;rom&#xe8;tre">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883441874" ID="ID_1987848506" MODIFIED="1516964042120" POSITION="left" TEXT="Contacteur">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883872082" ID="ID_316231831" MODIFIED="1516964064705" POSITION="right" TEXT="Capteur de petites vibrations">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883884216" ID="ID_1727903434" MODIFIED="1516964043720" POSITION="left" TEXT="Capteur TILT">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883891680" ID="ID_1688085615" MODIFIED="1516964062737" POSITION="right" TEXT="Capteur de flexion">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883955290" ID="ID_121714290" MODIFIED="1516964045224" POSITION="left" TEXT="Capteur de flexion">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883966072" ID="ID_1580515228" MODIFIED="1516964060865" POSITION="right" TEXT="D&#xe9;tecteur d&apos;obstacle">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1516883977393" ID="ID_1270479318" MODIFIED="1516964118414" POSITION="left" TEXT="Capteur pi&#xe9;zo&#xe9;lectrique">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516883998036" ID="ID_1804071904" MODIFIED="1516964058961" POSITION="right" TEXT="Capteur de mouvement PIR">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1516884016305" ID="ID_1513499904" MODIFIED="1516964049074" POSITION="left" TEXT="Capteur IR">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1516884022435" ID="ID_696894165" MODIFIED="1516964056432" POSITION="right" TEXT="Capteur ultrason">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1516884035474" ID="ID_368239113" MODIFIED="1516964050546" POSITION="left" TEXT="Capteur d&apos;angle">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1516884049051" ID="ID_1184190463" MODIFIED="1516964054394" POSITION="right" TEXT="Capteur effet hall">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+</map>
