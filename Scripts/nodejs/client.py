a47b0d68b1ff22fe788daad48585aaddd250642a
Noah Lanfrit <noah.lanfrit@hotmail.com>
2018-04-24 09:47:59 +0200

    maj

---
+++ Scripts/nodejs/client.py
@@ -0,0 +1,18 @@
+from socketIO_client import SocketIO, LoggingNamespace
+
+def on_connect():
+    print('connect')
+
+def on_disconnect():
+    print('disconnect')
+
+def on_reconnect():
+    print('reconnect')
+
+def on_aaa_response(*args):
+    print('on_aaa_response', args)
+
+socketIO = SocketIO('127.0.0.1', 5000, LoggingNamespace)
+socketIO.on('connect', on_connect)
+socketIO.on('disconnect', on_disconnect)
+socketIO.on('reconnect', on_reconnect)
