ee86f553d70aed5cc2841501049fca1d61cdbe78
Noah Lanfrit <noah.lanfrit@hotmail.com>
2018-04-24 10:23:45 +0200

    maj

--- Scripts/nodejs/client.py
+++ Scripts/nodejs/client.py
@@ -1,5 +1,22 @@
 from socketIO_client import SocketIO, LoggingNamespace
 
-with SocketIO('http://78.246.39.51', 5000, verify=False) as socketIO:
-    socketIO.emit('aaa')
-    socketIO.wait(seconds=1)
+def on_connect():
+    print('connect')
+
+def on_disconnect():
+    print('disconnect')
+
+def on_reconnect():
+    print('reconnect')
+
+def on_aaa_response(*args):
+    print('on_aaa_response', args)
+
+socketIO = SocketIO('78.246.39.51', 5000, LoggingNamespace)
+
+while True:
+	socketIO.on('return client connection', on_connect)
+	socketIO.on('disconnect', on_disconnect)
+	socketIO.on('reconnect', on_reconnect)
+
+	socketIO.wait(seconds=1)
